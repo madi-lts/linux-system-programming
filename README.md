21e998a702586013371a1b455bb865cbddbccc84
madi-lts <madi.j.anderson@gmail.com>
2024-11-18 16:25:47 -0500

    ls

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Remaking well known linux programs.
 
 ## program_01: cat
+
+- Parses one or more command line arguments and options. 
+- Arguments are interpreted as filenames which are then read in and printed out to standard output.
+
+## program_02: ls
+
+- Prints all directories linked to any number of input files.
---
+++ program_02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <dirent.h>
+
+int main(int argc, char *argv[]) {
+    DIR *d;
+    struct dirent *f;
+    for (int ii = 0; ii < argc; ii++) {
+        d = opendir(argv[ii]);
+        if (d) {
+            while ((f = readdir(d)) != NULL) {
+                if (f->d_type == DT_DIR) {
+                    printf("%s\n", f->d_name);
+                }
+            }
+            closedir(d);
+        }
+    }
+    return 0;
+}
